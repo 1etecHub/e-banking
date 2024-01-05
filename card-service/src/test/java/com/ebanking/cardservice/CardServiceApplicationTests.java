9a25a7eac20eeba0573999cd7d278ad7c16653bf
Ebenezer <ebenirene5@gmail.com>
2024-01-05 11:03:07 +0100

    added the loans and card service module

---
+++ card-service/src/test/java/com/ebanking/cardservice/CardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanking.cardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ loan-service/src/test/java/com/ebanking/loanservice/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanking.loanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
