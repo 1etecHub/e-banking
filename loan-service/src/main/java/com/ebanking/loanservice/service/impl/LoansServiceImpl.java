a648b9efe066ab2ab945dc825330f53ac9b77f30
Ebenezer <ebenirene5@gmail.com>
2024-01-05 12:02:25 +0100

    added loans service implementation

---
+++ loan-service/src/main/java/com/ebanking/loanservice/service/impl/LoansServiceImpl.java
@@ -0,0 +1,74 @@
+package com.ebanking.loanservice.service.impl;
+
+
+import com.ebanking.loanservice.dto.LoansDto;
+import com.ebanking.loanservice.entity.Loans;
+import com.ebanking.loanservice.exception.LoanAlreadyExistsException;
+import com.ebanking.loanservice.exception.ResourceNotFoundException;
+import com.ebanking.loanservice.mapper.LoansMapper;
+import com.ebanking.loanservice.repository.LoansRepository;
+import com.ebanking.loanservice.service.LoansService;
+import com.ebanking.loanservice.util.LoansConstants;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class LoansServiceImpl implements LoansService {
+
+    private LoansRepository loansRepository;
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loans> optionalLoans= loansRepository.findByMobileNumber(mobileNumber);
+        if(optionalLoans.isPresent()){
+            throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber "+mobileNumber);
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new loan details
+     */
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
+
+    /**
+     *
+     * @param mobileNumber - Input mobile Number
+     * @return Loan Details based on a given mobileNumber
+     */
+    @Override
+    public LoansDto fetchLoan(String mobileNumber) {
+        Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        return LoansMapper.mapToLoansDto(loans, new LoansDto());
+    }
+
+    /**
+     *
+     * @param loansDto - LoansDto Object
+     * @return boolean indicating if the update of loan details is successful or not
+     */
+
+
+
+}
